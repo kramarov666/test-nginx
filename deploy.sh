b7f87f78f094af605cc5fc897e0be21625414711
Your Name <you@example.com>
2017-12-05 14:30:44 +0000

    12455

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,8 @@
+cont_name="test-nginx"
 docker-machine create --driver amazonec2 --amazonec2-open-port 80 --amazonec2-region us-east-1 aws-sandbox
 
 eval $(docker-machine env aws-sandbox)
 
 docker pull kramarov555/test-nginx
+docker stop $cont_name
+docker run -d -p 80:80 --name $cont_name kramarov555/test-nginx
