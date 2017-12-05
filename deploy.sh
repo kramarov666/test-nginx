cb08537ff6e4011601858186b4de7453734708fd
Your Name <you@example.com>
2017-12-05 14:55:12 +0000

    133

--- deploy.sh
+++ deploy.sh
@@ -1,8 +1,8 @@
 cont_name="test-nginx"
 ec2_name="test-nginx"
-docker-machine create --driver amazonec2 --amazonec2-open-port 80 --amazonec2-region us-east-1 $test-nginx
+docker-machine create --driver amazonec2 --amazonec2-open-port 80 --amazonec2-region us-east-1 $ec2_name
 
-eval $(docker-machine env $test-nginx)
+eval $(docker-machine env $ec2_name)
 
 docker pull kramarov555/test-nginx
 docker rm $cont_name
